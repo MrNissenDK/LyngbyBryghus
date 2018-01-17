413a3851fb98ad66843ee33a649bc09307d2a31b
Mads Nissen <mrnissen@live.dk>
2018-01-17 10:59:01 +0100

    Ændret produkt siden så den loader med tagne og addet animation på kategori ændring.

--- Lyngby Bryghus/Scripts/Main_JavaScript.js
+++ Lyngby Bryghus/Scripts/Main_JavaScript.js
@@ -1,13 +1,32 @@
-﻿function ShowProducts(Tag) {
-    if (Tag === "*") {
-        $("a.gallery_product").show();
-    }
-    else {
-        $("a.gallery_product").hide();
-        $("[data-tags*=\"" + Tag + "\"]").show();
-    }
-    
-}
+﻿$(function () {
+	$("a.cattegoryLink").click(function (e) {
+		e.preventDefault();
+		let ancher = $(this);
+		let Tag = ancher.attr("data-tag");
+		window.history.pushState(null, ancher.attr("href"), ancher.attr("href"));
+		if (Tag === "*") {
+			$("a.gallery_product").css("display", "block");
+			$("a.gallery_product").css("position", "relative");
+			$("a.gallery_product").animate({
+				opacity: 1,
+				display: "block"
+			}, 300);
+		}
+		else {
+			$("[data-tags*=\"" + Tag + "\"]").css("display", "block");
+			$("[data-tags*=\"" + Tag + "\"]").css("position", "relative");
+			$("[data-tags*=\"" + Tag + "\"]").animate({
+				opacity: 1
+			}, 300);
+			$("a.gallery_product:not([data-tags*=\"" + Tag + "\"])").css("position", "absolute");
+			$("a.gallery_product:not([data-tags*=\"" + Tag + "\"])").animate({
+				opacity: 0,
+			}, 300, function () {
+				$(this).css("display","none");
+			});
+		}
+	});
+});
 
 
 

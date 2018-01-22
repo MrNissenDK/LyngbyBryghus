a5d13a6a381aad7dcc8c58c49b43f934be950ed8
Mads Nissen <mrnissen@live.dk>
2018-01-22 11:02:05 +0100

    Finish wysiswyg

--- Lyngby Bryghus/Scripts/Main_JavaScript.js
+++ Lyngby Bryghus/Scripts/Main_JavaScript.js
@@ -26,10 +26,9 @@
 			});
 		}
 	});
-
-
 	tinymce.init({
 		selector: '#Editor textarea',
+		forced_root_block: "",
 		height: 467,
 		border: 0,
 		width: 498,
@@ -52,18 +51,39 @@
 	$("a.Edit").click(function (e) {
 		e.preventDefault();
 		let Editor = $("#Editor");
-		Editor.show();
 		let ancher = $(this);
 		let title = ancher.attr("data-Title");
 		let jPath = ancher.attr("data-jPath");
 		value = $(ancher.attr("data-target"));
-
 		Editor.find("form input[name=jPath]").val(jPath);
 		Editor.find("form .Title").text(title);
 
+		tinymce.remove();
+		tinymce.init({
+			selector: '#Editor textarea',
+			forced_root_block: value.prop("tagName"),
+			forced_root_block_attrs: { class: value.attr("class") },
+			height: 467,
+			border: 0,
+			width: 498,
+			verify_html: false,
+			plugins: [
+				"advlist autolink lists link image charmap print preview anchor",
+				"searchreplace visualblocks code fullscreen",
+				"insertdatetime media table paste imagetools wordcount"
+			],
+			toolbar: "insertfile undo redo | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image",
+			// imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+			content_css: [
+				'/bootstrap-3.3.7-dist/css/bootstrap.min.css',
+				'/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css',
+				'/CascadingStyleSheet/font-awesome-4.7.0/css/font-awesome.css'
+			]
+		});
 
 		tinymce.activeEditor.execCommand('mceSetContent', true, value[0].outerHTML);
 
+		setTimeout(function () { Editor.show();}, 1);
 	});
 	$("#Editor > form > button.close-button").click(function (e) {
 		let Editor = $("#Editor");

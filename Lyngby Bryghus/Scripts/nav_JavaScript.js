d2cfcd544da388e46ad557d5c36f06e6f2603fd9
cami5211 <cami5211@VID6VBHS32>
2018-01-24 22:35:35 +0100

    Rettet EN MASSE!! Edit-knap nu med baggrund, titler på alle faneblad, navbar a:hover skifter med side -ikke helt færdig, title-attribut på alle edit-knepperne.. :)

--- Lyngby Bryghus/Scripts/nav_JavaScript.js
+++ Lyngby Bryghus/Scripts/nav_JavaScript.js
@@ -5,3 +5,26 @@
         $('.opaque-navbar').removeClass('opaque');
     }
 });
+
+
+
+var urlname = window.location.href;
+
+// passes on every element with a class ".link"
+$(".link").each(function () {
+    // checks if its the same on the address bar
+    if (urlname == (this.href)) {
+        $(this).closest(".link").toggleClass("hovered");
+    }
+});
+
+
+$(".btn-edit").mouseover(function () {
+    $(this).closest("div.textbox").addClass("deco");
+
+
+});
+
+$(".btn-edit").mouseout(function () {
+    $(this).closest("div.textbox").removeClass("deco");
+});

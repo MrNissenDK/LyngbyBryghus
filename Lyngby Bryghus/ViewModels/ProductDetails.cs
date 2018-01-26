8dad5c0dccdba591a3bb155c7850b517c0d8bb14
Mads Nissen <mrnissen@live.dk>
2018-01-26 11:28:05 +0100

    se Næste

---
+++ Lyngby Bryghus/ViewModels/ProductDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lyngby_Bryghus.ViewModels
+{
+    public class ProductDetails
+    {
+        public int Next { get; set; }
+        public Repos.Models.Products product { get; set; }
+    }
+}
